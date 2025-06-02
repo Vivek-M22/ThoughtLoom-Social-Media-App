a2a684da36ae50ede612ff6eac376e2932320a5b
Vivek Makwana <vivekmakwana.one@gmail.com>
2025-06-02 17:40:03 +0530

    fix logo issue

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import logoImage from '../assets/TL_logo.png'
 
 function Logo({ width = '100px', height = 'auto' }) {
     return (
         <img 
-            src="/src/assets/TL_logo.png" 
+            src={logoImage}
             alt="ThoughtLoom" 
             style={{ width: width, height: height }}
         />
